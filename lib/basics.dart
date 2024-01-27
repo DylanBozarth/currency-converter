070d73dc2098154cc8e940bde4103a65af6d23b3
dylan <dylanabozarth@gmail.com>
2024-01-27 14:14:44 -0800

    flow

---
+++ lib/basics.dart
@@ -0,0 +1,25 @@
+//maybe a calcuator?
+
+//lists are arrays []
+// sets are an unordered list of unquie items {}. Will only add a certain thing once
+// maps are an object that assigns a key to each value {}  {'Tom': '150lbs'} key value pairs
+// assert = stop the program if ()   ex: assert(user.isAuth);
+//const must have a constant value, nothing can change. Final can have parts of it change before runtime.
+// dynamic will let you have the variable act like vanilla javascript.
+var maps = {'test': 'cheese'};
+var example = maps.remove('test');
+
+int doMath(operator) {
+  var returnNumber = 0;
+  switch (operator) {
+    case 'X':
+      break;
+    case '/':
+      break;
+    case '+':
+      break;
+    case '-':
+      break;
+  }
+  return returnNumber;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,32 @@
+import 'package:currencyconverter/basics.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MathMaker());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MathMaker extends StatefulWidget {
+  const MathMaker({super.key});
 
+  @override
+  State<MathMaker> createState() => _MathMakerState();
+}
+
+class _MathMakerState extends State<MathMaker> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Whats in my food?',
-            style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Arial'),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrangeAccent,
-        ),
-        floatingActionButton: Container(
-            width:
-                double.infinity, // Set width to occupy the entire screen width
-            margin: const EdgeInsets.all(16.0),
-            child: FloatingActionButton(
-              backgroundColor: Colors.green[600],
-              onPressed: () {},
-              child: const Text('SCAN'),
-            )),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.blueGrey, title: const Text('Do math')),
+            body: Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Colors.green,
+              height: 50,
+              width: 50,
+              child: const Text('1'),
+            )));
   }
 }
+// https://www.youtube.com/watch?v=1xipg02Wu8s 5:09 
