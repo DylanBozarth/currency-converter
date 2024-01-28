2a74c3cafd37fb2433f0bae32d92d6175bdd0c5f
dylan <dylanabozarth@gmail.com>
2024-01-28 14:55:29 -0800

    lay-it-out

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,63 @@ class _MathMakerState extends State<MathMaker> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                backgroundColor: Colors.blueGrey, title: const Text('Do math')),
-            body: Container(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+            backgroundColor: Colors.blueGrey, title: const Text('Do math')),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.green,
               margin: const EdgeInsets.all(10),
               padding: const EdgeInsets.all(10),
-              color: Colors.green,
               height: 50,
               width: 50,
-              child: const Text('1'),
-            )));
+              child: const Text('1'), //Selector number 1
+            ),
+            Container(
+              color: Colors.green,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              height: 50,
+              width: 50,
+              child: const Text('2'), // selector number 2
+            ),
+            const Column(
+              children: [
+                Text('buttons go here'),
+              ],
+            ),
+            const Row(
+              children: [
+                Center(
+                    child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('+'),
+                )),
+                Center(
+                    child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('-'),
+                )),
+                Center(
+                    child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('X'),
+                )),
+                Center(
+                    child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('%'),
+                )),
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add), onPressed: () {}),
+      ),
+    );
   }
 }
 // https://www.youtube.com/watch?v=1xipg02Wu8s 5:09 
